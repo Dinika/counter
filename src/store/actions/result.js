dfced0c9e8b12c387bfeda5fea51b2aecb1f56d4
Dinika <dinikasaxenas@gmail.com>
2019-02-15 22:46:37 +0530

    organize action creators in separate files
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

---
+++ src/store/actions/result.js
@@ -0,0 +1,17 @@
+import * as actionTypes from './actionTypes';
+
+const saveResult = result => ({
+  type: actionTypes.STORE_RESULT,
+  result
+});
+
+export const storeResult = result => {
+  return dispatch => {
+    setTimeout(() => dispatch(saveResult(result)), 2000);
+  };
+};
+
+export const deleteResult = id => ({
+  type: actionTypes.DELETE_RESULT,
+  resultElId: id
+});
